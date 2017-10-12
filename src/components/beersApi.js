07733722d34f36d6107a92457f9d25641e3cdeac
agandia9 <alejandro.gandia9@gmail.com>
2017-10-12 13:18:44 +0200

    first commit

---
+++ src/components/beersApi.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+const beersApi =  {
+	baseUrl: 'https://quiet-inlet-67115.herokuapp.com/api',
+
+	searchBeers: function (query) {
+		return axios.get(this.baseUrl + '/search/all?q=' + query)
+			.then(({data}) => { return data})
+	},
+
+	getBeer: function (id, callback) {
+		return axios.get(this.baseUrl + '/beer/' + id)
+	}
+}
+
+export default beersApi
+
